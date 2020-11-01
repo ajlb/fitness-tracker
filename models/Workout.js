3b131e2df130ab3d259b616a7a12ecbcbd74ebb7
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-10-31 21:27:32 -0700

    update models

--- models/Workout.js
+++ models/Workout.js
@@ -1,17 +1,45 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workoutSchema = new Schema ({
     day: {
         type: Date,
-        default: Date.now
+        default: () => new Date()
     },
-    exersizes: {
-        type: Schema.Types.ObjectId,
-        ref: 'excercise'
-    }
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter your exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter yor exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter your exercise duration in minutes"
+            },
+            distance: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            }
+        }
+    ]
 });
-const Workout = mongoose.model('workout', workoutSchema);
 
-module.exports =  Workout;
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
 
